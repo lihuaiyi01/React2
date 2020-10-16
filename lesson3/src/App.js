98922f4d90701cc7777a54f18790280e0222f2d5
lihuaiyi01 <1195841709@qq.com>
2020-10-16 13:31:31 +0800

    111

--- lesson3/src/App.js
+++ lesson3/src/App.js
@@ -2,10 +2,12 @@ import React, { useState } from 'react';
 import ReactReduxPage from "./pages/ReactReduxPage";
 
 function App() {
+  const [num, setNum] = useState(0);
   return (
     <div className="App">
+      <button>change num</button>
       {/* react redux学习 */}
-      <ReactReduxPage msg="msg" />
+      <ReactReduxPage num="num" />
     </div>
   );
 }
--- lesson3/src/pages/ReactReduxPage.js
+++ lesson3/src/pages/ReactReduxPage.js
@@ -4,13 +4,24 @@ import { connect } from "react-redux";
 // connect 链接store与组件 其实这里返回的是一个新的组件 hoc
 export default connect(
   // mapStateToProps Function (state, ownProps)
-  // state => ({ count: state })
-  (state, ownProps) => {
-    console.log("ownProps", ownProps);
-    return {
-      count: state
-    };
+  state => ({ count: state }),
+  // ownProps是组件本身的props
+  // ！ownProps谨慎使用，如果ownProps发生变化的话，mapStateToProps会被重新执行，
+  // ！state也会被重新计算，这个时候影响性能
+  // (state, ownProps) => {
+  //   console.log("ownProps", ownProps);
+  //   return {
+  //     count: state
+  //   };
+  // }
+
+  // mapDispatchToProps Object
+  // 如果不指定mapDispatchToProps， 默认props会被注入dispatch本身
+  // object ，dispatch本身不会被注入props
+  {
+    add: () => ({ type: 'ADD' })
   }
+
 )(
   class ReactReduxPage extends Component {
     render() {
