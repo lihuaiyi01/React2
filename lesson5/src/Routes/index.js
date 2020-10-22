47432b583759e7876fd24e2d2859f297e1eae436
lihuaiyi01 <1195841709@qq.com>
2020-10-22 16:17:13 +0800

    111

---
+++ lesson5/src/Routes/index.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route
+} from "react-route-dom";
+
+export default function Routes(props) {
+  return (
+    <div>
+      <h3>Routes</h3>
+    </div>
+  );
+}
